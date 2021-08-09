8aa9da6d6f0c733953dffb25c7e52b330d7e9147
Karen Baliero <karenbaliero.lima@gmail.com>
2021-08-08 22:44:50 -0300

    Add project1

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Karen Lima on 07/08/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var index: Int?
+    var total: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Picture \(index!) of \(total!)"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+}
