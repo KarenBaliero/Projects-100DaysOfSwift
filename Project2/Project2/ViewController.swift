477c48c7171862a2b3cf957cb0b133a6414c72fe
Karen Baliero <karenbaliero.lima@gmail.com>
2021-08-10 21:13:02 -0300

    feature: add game logic
    Implementation of score using alert to show and random for showing flags.

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -15,9 +15,11 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        countries += ["Estonia", "France", "Germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        countries += ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
@@ -28,12 +30,34 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion(){
+    func askQuestion(action: UIAlertAction! = nil){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        title =  countries[correctAnswer].uppercased()
     }
-
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer{
+            title = "Correct"
+            score+=1
+        } else{
+            title = "Wrong"
+            score-=1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .cancel, handler: askQuestion))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
+        
+        present(ac, animated: true)
+    }
+    
 
 }
 
