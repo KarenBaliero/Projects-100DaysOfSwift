afc81687ddf0ab5f01b1515fddf7aa8c2c514f71
Karen Baliero <karenbaliero.lima@gmail.com>
2021-08-12 21:10:38 -0300

    feature: quiz questions limit and visible score
    Implementation of a limite of questions, when its equal to 5 it shows the final score and restarts

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var answeredQuestions = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,31 +32,41 @@ class ViewController: UIViewController {
     }
     
     func askQuestion(action: UIAlertAction! = nil){
+        if answeredQuestions > 5 {
+            title = "Game Over"
+            let ac = UIAlertController(title: title, message: "Your final score is \(score)", preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "Try Again", style: .cancel, handler: askQuestion))
+            
+            present(ac, animated: true)
+            score = 0
+            answeredQuestions = 0
+        }
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
-        title =  countries[correctAnswer].uppercased()
+        title =  countries[correctAnswer].uppercased() + " - SCORE: \(score)"
     }
     
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
+        answeredQuestions+=1
         if sender.tag == correctAnswer{
-            title = "Correct"
             score+=1
+            askQuestion()
         } else{
-            title = "Wrong"
+            title = "Oops"
             score-=1
+            let ac = UIAlertController(title: title, message: "Wrong! That's the flag of \(countries[sender.tag])", preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "Continue", style: .cancel, handler: askQuestion))
+            present(ac, animated: true)
+            
         }
         
-        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
-        
-        ac.addAction(UIAlertAction(title: "Continue", style: .cancel, handler: askQuestion))
-        ac.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
-        
-        present(ac, animated: true)
     }
     
 
