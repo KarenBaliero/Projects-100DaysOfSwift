ba02b0b1516336017f34e86eb2fd396311700da4
Karen Baliero <karenbaliero.lima@gmail.com>
2021-08-21 18:21:31 -0300

    Project6b

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -11,7 +11,89 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = .red
+        label1.text = "THESE"
+        label1.sizeToFit()
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = .cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = .yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = .green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = .orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5 ]
+//        // H: horizonal
+//        //| edge of the view
+//        // [ ] put the label here
+//        // viewsDictionary reference
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//
+//        let metrics = ["labelHeight": 88]
+//        // V: vertical
+//        // - a space 10 points by default
+//        // without |  at the end so we have an empty space at the end
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+//
+        var previous: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5]{
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            //let height = view.frame.size.height/5 - 15
+            //label.heightAnchor.constraint(equalToConstant: height).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+//            if UIDevice.current.orientation.isLandscape {
+//                label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+//                label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+//            } else{
+//                label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+//                label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+//            }
+//            else if UIDevice.current.orientation.isFlat {
+//
+//            } else if UIDevice.current.orientation.isPortrait {
+//
+//            } else if UIDevice.current.orientation.isValidInterfaceOrientation {
+//
+//            }
+            
+            previous = label
+            
+        }
+        
     }
 
 
