1e46040c0f9a9471cc4eff75ef139a7036b5117f
Karen Baliero <karenbaliero.lima@gmail.com>
2021-08-29 14:03:41 -0300

    Add project 1, 2 and 3 screenshots

---
+++ Project3/README.md
@@ -0,0 +1,20 @@
+# Project 3 - Social Media
+
+https://www.hackingwithswift.com/100/22
+
+Includes solutions to the [challenges](https://www.hackingwithswift.com/read/3/3/wrap-up). 
+
+## Topics
+
+UIBarButtonItem, UIActivityController
+
+## Challenge
+
+From [Hacking with Swift](https://www.hackingwithswift.com/read/3/3/wrap-up):
+>1. Try adding the image name to the list of items that are shared. The activityItems parameter is an array, so you can add strings and other things freely. Note: Facebook won’t let you share text, but most other share options will.
+>2. [Challenge 2](Challenge2/)
+
+## Screenshots
+
+![screenshot1](screenshots/screen01.png)
+![screenshot2](screenshots/screen02.png)
---
+++ Project9/Project7/ViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Karen Lima on 23/08/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var petitions = [Petition]()
+    var originalPetitions = [Petition]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Search", style: .plain, target: self, action: #selector(stringSearch))
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+    }
+    
+    @objc func fetchJSON() {
+        let urlString: String
+        if navigationController?.tabBarItem.tag == 0{
+            //urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+                urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+
+        //blocking code
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url){
+                parse(json: data)
+                return
+            }
+        }
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            originalPetitions = jsonPetitions.results
+            petitions = jsonPetitions.results
+            tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        } else{
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+        }
+    }
+    
+    @objc func showError(){
+        let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection;", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func stringSearch(){
+        let ac = UIAlertController(title: "Enter search word:", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAcion = UIAlertAction(title: "Submit", style: .default ) {
+            [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.filterPetitions(word: answer)
+        }
+        
+        ac.addAction(submitAcion)
+        present(ac, animated: true)
+    }
+    
+    @objc func showCredits(){
+        let ac = UIAlertController(title: "Credits", message: "The data comes from the We The People API of the Whitehouse.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true)
+        
+    }
+    
+    func filterPetitions(word searchWord: String) {
+        var filteredPetitions = [Petition]()
+        if searchWord.isEmpty{
+            return
+        }
+        for petition in self.originalPetitions{
+            if petition.body.contains(searchWord){
+                filteredPetitions.append(petition)
+            }
+        }
+        self.petitions = filteredPetitions
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+
+}
+
