2107c4aa1c21877835cd67ddbc14b87e8db3a756
Karen Baliero <karenbaliero.lima@gmail.com>
2021-09-02 18:32:24 -0300

    Add project 10

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -5,29 +5,88 @@
 //  Created by Karen Lima on 01/09/21.
 //
 
+
 import UIKit
 
-class ViewController: UICollectionViewController {
-
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var people = [Person]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("carregou")
-        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print("carregou2")
-        return 10
+        return people.count
     }
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        print("carregou3")
         guard let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
             fatalError("Unable to dequeue PersonCell.")
         }
-        print("carregou4")
+        print("aqui1")
+        let person = people[indexPath.item]
+        
+        cell.name.text = person.name
+        
+        let path = getDocumentDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
         
         return cell
     }
-
+    
+    @objc func addNewPerson (){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+        print("aqui2")
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("aqui5")
+        guard let image = info[.editedImage] as? UIImage else { return }
+        print("aqui4")
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8){
+            try? jpegData.write(to: imagePath)
+        }
+        print("aqui3")
+        
+        let person = Person(name: "Unknow", image: imageName)
+        people.append(person)
+        collectionView?.reloadData()
+        dismiss(animated: true)
+    }
+    
+    func getDocumentDirectory() -> URL{
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.row]
+        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+        
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "OK", style: .default){
+            [weak self, weak ac] _ in
+            guard let newName = ac?.textFields?[0].text else {return}
+            person.name = newName
+            self?.collectionView.reloadData()
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+ 
 }
 
