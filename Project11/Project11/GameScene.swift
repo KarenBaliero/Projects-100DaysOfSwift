e35ccbfdda2e54a88e4e63bfeec5ebab8501c664
Karen Baliero <karenbaliero.lima@gmail.com>
2021-09-04 14:01:27 -0300

    Add project 11

---
+++ Project11/Project11/GameScene.swift
@@ -0,0 +1,49 @@
+//
+//  GameScene.swift
+//  Project11
+//
+//  Created by Karen Lima on 04/09/21.
+//
+
+import SpriteKit
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        makeBouncer(at: CGPoint(x: 0, y: 0))
+        makeBouncer(at: CGPoint(x: 256, y: 0))
+        makeBouncer(at: CGPoint(x: 512, y: 0))
+        makeBouncer(at: CGPoint(x: 768, y: 0))
+        makeBouncer(at: CGPoint(x: 1024, y: 0))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let ball = SKSpriteNode(imageNamed: "ballRed")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
+        
+//        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+//        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+//        box.position = location
+//        addChild(box)
+    }
+    
+    func makeBouncer(at position: CGPoint){
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
+    }
+}
