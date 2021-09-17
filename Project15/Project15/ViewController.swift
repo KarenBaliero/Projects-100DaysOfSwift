c673f811a522f5314093136e43eed0a99143b883
Karen Baliero <karenbaliero.lima@gmail.com>
2021-09-16 22:49:01 -0300

    Add project 15:Animation

---
+++ Project15/Project15/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Project15
+//
+//  Created by Karen Lima on 16/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
+        
+    }
+
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        //UIView.animate(withDuration: 1, delay: 0, options:[]) {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options:[], animations: {
+            switch self.currentAnimation {
+            case 0:
+                //só executa uma vez pois percebe que na segunda ja esta na escala do codigo
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                break
+            case 1:
+                //retorna as configuracoes padroes
+                self.imageView.transform = .identity
+                break
+            case 2:
+                //sao dados em relacao a posicao atual
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+                break
+            case 3:
+                self.imageView.transform = .identity
+                break
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+                break
+            case 5:
+                self.imageView.transform = .identity
+                break
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = .green
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
+            default:
+                break
+            }
+        }, completion: { finish in
+            sender.isHidden = false
+        })
+
+        currentAnimation += 1
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
+}
+
