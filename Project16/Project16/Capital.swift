a3c514d2b1c5dd61cd75e753f94cd72a17e2ab57
Karen Baliero <karenbaliero.lima@gmail.com>
2021-09-19 14:22:20 -0300

    Add project16 using MapKit

---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,20 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Karen Lima on 19/09/21.
+//
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String){
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
