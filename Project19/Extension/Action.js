a03243aed527b6933a4521546dedee36b63a7378
Karen Baliero <karenbaliero.lima@gmail.com>
2021-09-27 18:07:55 -0300

    Add project19 keyboard and UITextView

--- Project19/Extension/Action.js
+++ Project19/Extension/Action.js
@@ -3,11 +3,13 @@ var Action = function() { }
 Action.prototype = {
 //before loading
 run: function(parameters){
-    
+    parameters.completionFunction({"URL": document.URL, "title": document.title});
 },
 //after loading
 finalize: function(parameters){
-    
+    var customJavaScript = parameters["customJavaScript"]
+    //will run the script
+    eval(customJavaScript)
 }
 };
 
--- Project19/Extension/ActionViewController.swift
+++ Project19/Extension/ActionViewController.swift
@@ -11,31 +11,67 @@ import UniformTypeIdentifiers
 
 class ActionViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
-
+    @IBOutlet var script: UITextView!
+    var pageTitle = ""
+    var pageURL = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         //we got a extension context. array of data passed, we get the first one.
         //lets us control how it interacts with the parent app
         //inputItems is an array of data the parent app is sending to our extension to use
-        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+        if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
             //this contains an array of attachments, are given to us wrapped up as an NSItemProvider
             //the first attachment of the first item
             if let itemProvider = inputItem.attachments?.first {
                 // to ask the item provider to actually provide us with its item
                 //l [weak self] to avoid strong reference cycle
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String){ [weak self] (dict, error) in
+                    guard let itemDictionary = dict as? NSDictionary else { return }
+                    guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
                     
-                    //do stuff
+                    self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                    self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+                    
+                    DispatchQueue.main.async {
+                        self?.title = self?.pageTitle
+                    }
                 }
             }
         }
     }
 
     @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text]
+        let webDicitionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDicitionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+        extensionContext?.completeRequest(returningItems: [item])
+//        // Return any edited content to the host app.
+//        // This template doesn't do anything, so we just echo the passed in items.
+//        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = .zero
+        } else {
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        script.scrollIndicatorInsets = script.contentInset
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
     }
 
 }
